11e6f29acf978865277efc1747111307dcb1d379
Inna Mykytiuk <innka-pinnka@ukr.net>
2025-01-21 19:36:34 +0200

    add simple server

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,8 @@
+
+fetch("http://localhost:3001/users").then((res) => {
+  console.log(res)
+});
+
 export default function App() {
   return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
 }
