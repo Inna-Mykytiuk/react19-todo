1ac3530183a657b916ba4a02f6846f96eb440817
Inna Mykytiuk <innka-pinnka@ukr.net>
2025-02-10 19:58:06 +0200

    code refactoring

--- src/components/CreateTaskForm.tsx
+++ src/components/CreateTaskForm.tsx
@@ -4,26 +4,25 @@
 export function CreateTaskForm() {
 
 
+    return (
+        <form className="flex gap-2"
 
-  return (
-    <form className="flex gap-2"
-
-    >
-      <input
-        name="email"
-        type="email"
-        className="border p-2 rounded w-full"
-        placeholder="Enter email"
+        >
+            <input
+                name="email"
+                type="email"
+                className="border p-2 rounded w-full"
+                placeholder="Enter email"
 
 
-      />
-      <button
-        type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-400"
-      >
-        Submit
-      </button>
-      <div className="text-red-600">Еrror</div>
-    </form>
-  );
+            />
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-400"
+            >
+                Submit
+            </button>
+            <div className="text-red-600">Еrror</div>
+        </form>
+    );
 }
